748206a52ff7299b0d05f7791c5cdcb37d6d1826
Juan Esteban Méndez <je.mendez@uniandes.edu.co>
2019-09-15 12:59:08 -0500

    Se agregaron las operaciones POST, PUT y DELETE.

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const MongoClient = require("mongodb").MongoClient;
 
+const bodyParser = require("body-parser");
 const app = express();
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 const url = "mongodb://localhost:27017"
 
@@ -23,11 +27,41 @@ function getCountryByName(countryName, callback){
     conn.then(client=>{
         let res = client.db("population").collection("countries").find({"country": countryName}).toArray();
         res.then(value => {
-            //paises = [];
             pais = value;
             callback(pais);
         });
     });    
+}
+
+function postCountry(country, callback){
+    conn.then(client=>{
+        let res = client.db("population").collection("countries").insertOne(country);
+        res.then(value => {
+            pais = value;
+            callback(pais);
+        });
+    });   
+}
+
+function editCountry(country, content, callback){
+    conn.then(client=>{
+        let res = client.db("population").collection("countries").updateOne({"country" : country}, {$set: content});
+        res.then(value => {
+            pais = value;
+            callback(pais);
+        });
+    }).catch(reason => {
+        console.log(reason);
+    });   
+}
+
+function deleteCountry(country, callback){
+    conn.then(client=>{
+        let res = client.db("population").collection("countries").deleteOne({"country" : country});
+        res.then(value => {
+            callback(value);
+        });
+    });
 }
 
 app.get('/', (req, res) => {
@@ -47,6 +81,36 @@ app.get('/countries/:country', (req, res) => {
     });
 });
 
+app.post('/countries', (req, res) => {
+    let countryToPost = {
+        "country" : req.body.country,
+        "population" : req.body.population,
+        "continent" : req.body.continent,
+        "lifeExpectancy" : req.body.lifeExpectancy,
+        "purchasingPower" : req.body.purchasingPower
+    };
+    console.log(countryToPost);
+    postCountry(countryToPost, (data) => {
+        res.json(data);
+    });
+});
 
+app.put('/countries/:country', (req, res) => {
+    let content = {
+        "population" : req.body.population,
+        "continent" : req.body.continent,
+        "lifeExpectancy" : req.body.lifeExpectancy,
+        "purchasingPower" : req.body.purchasingPower
+    };
+    editCountry(req.params.country, content, (data) => {
+        res.json(data);
+    });
+});
+
+app.delete('/countries/:country', (req, res) => {
+    deleteCountry(req.params.country, (data)=>{
+        res.json(data);
+    });
+});
 
 app.listen('8080');
